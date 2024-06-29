9e6a5b590098bbda4e34c92c82831aeae714fa0f
Dafik-G-2 <gajulapallidafik88776@gmail.com>
2024-06-29 12:37:22 +0530

    2nd commit

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-# resource "aws_security_group" "sg" {
-#     name = ""
-#     description = ""
-#     vpc_id = var.vpc_id
-# }
-
 resource "aws_instance" "my-instance" {
     ami = "ami-04f8d7ed2f1a54b14"
   instance_type = "t2.micro"
